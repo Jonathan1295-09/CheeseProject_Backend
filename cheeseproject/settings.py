97bf2ec10823fee4bdca7a4098f4ca55f5ef7cea
Jonathan1295-09 <jcasasola955@gmail.com>
2023-09-21 22:22:26 -0500

    secret key update

--- cheeseproject/settings.py
+++ cheeseproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v=7bmob0#6r#8&c3nmdmk5rey17&vjrfanusaa+%#&d2=-$i_k'
+SECRET_KEY = os.environ.get("SECRET_KEY", "localkey2023")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

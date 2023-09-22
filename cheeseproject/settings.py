e40d14944a486535b145b137f0f8841337cec386
Jonathan1295-09 <jcasasola955@gmail.com>
2023-09-21 22:37:55 -0500

    setup.sh and setting.py update

--- cheeseproject/settings.py
+++ cheeseproject/settings.py
@@ -29,6 +29,12 @@ DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
+## Handling Allowed Hosts on Render
+## add the render.com hostname to ALLOWED_HOSTS
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 

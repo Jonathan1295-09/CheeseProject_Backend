4089d1fe59783572ab1471a5e9abb11a0490867c
Jonathan1295-09 <jcasasola955@gmail.com>
2023-09-21 22:20:26 -0500

    serializer,views,settings,urls.pys updated

--- cheese/serializers.py
+++ cheese/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class CheeseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Cheese
-        fields = ('id','name','origin country','type')
+        fields = ('id', 'name', 'origin_country', 'type')
--- cheeseproject/urls.py
+++ cheeseproject/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from cheese.views import CheeseViewSet
+
+router = routers.DefaultRouter()
+router.register(r"cheese", CheeseViewSet)
+
+print(router.urls)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path("admin/", admin.site.urls)
 ]
